3d40b0ddd1a77fcbf328827a80374f9b299263a2
Arjan Filius <iafilius@xs4all.nl>
2026-07-26 19:50:08 +0200

    feat(sec-agent): bump to v2.1.5

--- Formula/sec-agent.rb
+++ Formula/sec-agent.rb
@@ -1,9 +1,9 @@
 class SecAgent < Formula
   desc "macOS Enclave-Bound Session Agent for Encrypted Secrets"
   homepage "https://github.com/iafilius/sec-agent"
-  url "https://github.com/iafilius/sec-agent/releases/download/v2.1.4/sec-agent_v2.1.4_darwin_arm64.tar.gz"
-  version "2.1.4"
-  sha256 "c77cc6cb80bae52c801035ee3057ab0a86a2e23b40dceab59ea57895728c29f4"
+  url "https://github.com/iafilius/sec-agent/releases/download/v2.1.5/sec-agent_v2.1.5_darwin_arm64.tar.gz"
+  version "2.1.5"
+  sha256 "7f7136cdab8cf86a8809a64aa904f3aaf8c172e1b0454ec906148cb8111bc2de"
   license "MIT"
 
   depends_on :macos
@@ -14,6 +14,6 @@ class SecAgent < Formula
   end
 
   test do
-    assert_match "v2.1.4", shell_output("#{bin}/sec-agent version")
+    assert_match "v2.1.5", shell_output("#{bin}/sec-agent version")
   end
 end
