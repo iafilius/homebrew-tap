b363a66c9385d8c73d2fea3793bef570562acde7
Arjan Filius <iafilius@xs4all.nl>
2026-09-22 11:31:26 +0200

    chore(formula): release sec-agent v2.13.3

--- Formula/sec-agent.rb
+++ Formula/sec-agent.rb
@@ -1,9 +1,9 @@
 class SecAgent < Formula
   desc "macOS Enclave-Bound Session Agent for Encrypted Secrets"
   homepage "https://github.com/iafilius/sec-agent"
-  url "https://github.com/iafilius/sec-agent/releases/download/v2.13.2/sec-agent_v2.13.2_darwin_arm64.tar.gz"
-  version "2.13.2"
-  sha256 "d9b3e3faf01c84af7e20ef7b70936aa27ea9804c025ba5f800dc2f15c2ae3210"
+  url "https://github.com/iafilius/sec-agent/releases/download/v2.13.3/sec-agent_v2.13.3_darwin_arm64.tar.gz"
+  version "2.13.3"
+  sha256 "275560f5314d8dfc9668a5c13adca6d99b383e1aff635a6815c74980b7a118b4"
   license "GPL-3.0-or-later"
 
   depends_on :macos
@@ -11,6 +11,10 @@ class SecAgent < Formula
   def install
     bin.install "sec-agent"
     bin.install_symlink bin/"sec-agent" => "sec"
+  end
+
+  def post_install
+    system "#{bin}/sec-agent", "restart", "--hot-reload" rescue nil
   end
 
   test do
